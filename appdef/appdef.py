d93bb311600ae065603488f2634c3e384e02e35b
zhangjun <ibmzhangjun@139.com>
2024-02-14 21:52:45 +0800

    Init

--- appdef/appdef.py
+++ appdef/appdef.py
@@ -11,7 +11,6 @@
 
 import os
 import traceback
-from utils.log import log as log
 import simplejson as json
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -35,24 +34,28 @@ def singleton(cls):
 class Appdef():
     def __init__(self):
         self.AppName = None
-        self.Config = None
-        self.DBConnecton =None
+        self.Def = None
+        self.Defdict = None
         self.readconfig()
 
     def readconfig(self):
         try:
-            with open(DEF_PATH, 'r') as appdef_file:
-                appdef_data = json.load(appdef_file, object_hook=obj)
-                self.AppName = appdef_data.AppName
-                self.Config = appdef_data.Config
-                self.DBConnecton = appdef_data.DBConnecton
+            with open(DEF_PATH, 'r', encoding="utf-8") as appdef_file:
+                defcontent = appdef_file.read()
+            appdef_obj = json.loads(defcontent, object_hook=obj)
+            self.AppName = appdef_obj.AppName
+            self.Def = appdef_obj
+            appdef = json.loads(defcontent)
+            self.Defdict = appdef
         except Exception as exp:
-            log.error('Exception at Appdef.readconfig() %s ' % exp)
+            print('Exception at Appdef.readconfig() %s ' % exp)
             traceback.print_exc()
 
 
 if __name__ == '__main__':
     appdef = Appdef()
-    log.debug(appdef.AppName)
-    log.debug(appdef.Config)
-    log.debug(appdef.DBConnecton.Type)
+    print(appdef.AppName)
+    print(appdef.Def.Config)
+    print(appdef.Def.DBConnecton.Type)
+    print(appdef.Defdict)
+    print(appdef.Def.Config.API_Prefix[1:])
--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-
 #  #
 #  Copyright (C) 2023 ZinoHome, Inc. All Rights Reserved
 #  #
@@ -8,8 +7,62 @@
 #  @Author  : Zhang Jun
 #  @Email   : ibmzhangjun@139.com
 #  @Software: SwiftApp
+import os
 
 from pysondb import PysonDB
+from fastapi import FastAPI
+from appdef.appdef import Appdef
+from starlette_exporter import PrometheusMiddleware, handle_metrics
+from jaeger_client import Config as jaeger_config
+from opentracing.scope_managers.contextvars import ContextVarsScopeManager
+from opentracing_instrumentation.client_hooks import install_all_patches
+from starlette_opentracing import StarletteTracingMiddleWare
+from utils.log import log as log
 
+appdef = Appdef()
 
-db = PysonDB('appdef/app.json')
+# API prefix
+apiprefix = appdef.Def.Config.API_Prefix
+if apiprefix is not None:
+    if apiprefix.startswith('/'):
+        pass
+    else:
+        apiprefix = '/' + apiprefix
+else:
+    apiprefix = '/swiftapp'
+app = FastAPI(debug=appdef.Def.Config.debug,
+              title=appdef.Def.AppName,
+              description=appdef.Def.Description,
+              version=appdef.Def.Version,
+              openapi_url=apiprefix + "/openapi.json",
+              docs_url=None,
+              redoc_url=None
+              )
+# starlette_exporter
+app.add_middleware(
+    PrometheusMiddleware,
+    app_name=appdef.Def.AppName,
+    prefix=apiprefix[1:],
+    labels={
+        "server_name": os.getenv("HOSTNAME"),
+    },
+    group_paths=True,
+    buckets=[0.1, 0.25, 0.5],
+    skip_paths=['/health'],
+    always_use_int_status=False)
+app.add_route("/metrics", handle_metrics)
+
+# jaeger_tracer
+opentracing_config = jaeger_config(
+    config={
+        "sampler": {"type": "const", "param": 1},
+        "logging": True,
+        "local_agent": {"reporting_host": appdef.Def.Config.jaeger_host},
+    },
+    scope_manager=ContextVarsScopeManager(),
+    service_name="SwiftBuilder-" + appdef.Def.AppName,
+)
+jaeger_tracer = opentracing_config.initialize_tracer()
+install_all_patches()
+app.add_middleware(StarletteTracingMiddleWare, tracer=jaeger_tracer)
+
