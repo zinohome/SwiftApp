c10d46ea7222379b9bec3d3caf53d4c576e54b9b
zhangjun <ibmzhangjun@139.com>
2024-02-15 01:43:11 +0800

    Init

---
+++ core/settings.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#  #
+#  Copyright (C) 2023 ZinoHome, Inc. All Rights Reserved
+#  #
+#  @Time    : 2023
+#  @Author  : Zhang Jun
+#  @Email   : ibmzhangjun@139.com
+#  @Software: SwiftApp
+
+import os
+from pathlib import Path
+from typing import List
+from fastapi_amis_admin.admin.settings import Settings as AmisSettings
+from appdef.appdef import Appdef
+
+appdef = Appdef()
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+class Settings(AmisSettings):
+    name: str = appdef.Def.AppName
+    host: str = '0.0.0.0'
+    port: int = int(appdef.Def.Config.listenport)
+    debug: bool = appdef.Def.Config.debug
+    secret_key: str = ''
+    version: str = appdef.Def.Version
+    site_title: str = appdef.Def.AppName
+    site_icon: str = '/static/favicon.ico'
+    language: str = appdef.Def.Config.language
+    amis_cdn: str = '/static/'
+    amis_theme: str = 'cxd'
+    allow_origins: List[str] = None
+
+
+settings = Settings(_env_file=os.path.join(BASE_DIR, '.env'))
+
+
+if __name__ == '__main__':
+    print(settings.database_url_async)
+    print(settings.allow_origins)
