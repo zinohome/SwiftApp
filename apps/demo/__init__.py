72911594445012e869fa80ea38fc6fa282833609
zhangjun <ibmzhangjun@139.com>
2024-02-15 12:29:46 +0800

    Init

---
+++ apps/demo/__init__.py
@@ -0,0 +1,13 @@
+from fastapi import APIRouter
+from fastapi_amis_admin.admin import AdminApp
+
+
+def setup(router: APIRouter, admin_app: AdminApp, **kwargs):
+    # 导入相关模块
+    from . import admin, apis, jobs
+
+    # 注册路由
+    router.include_router(apis.router, prefix='/demo', tags=['Demo'])
+    # 注册管理页面
+    admin_app.register_admin(admin.DemoApp)
+
---
+++ main.py
@@ -0,0 +1,39 @@
+from starlette.responses import RedirectResponse
+from fastapi import FastAPI
+from sqlmodel import SQLModel
+
+from core.globals import site
+from core.settings import settings
+
+app = FastAPI(debug=settings.debug)
+
+# 安装应用
+from apps import demo
+demo.setup(app.router,site)
+
+# 挂载后台管理系统
+site.mount_app(app)
+
+# 注意1: site.mount_app会默认添加site.db的session会话上下文中间件,如果你使用了其他的数据库连接,请自行添加.例如:
+# from core.globals import sync_db
+# app.add_middleware(sync_db.asgi_middleware) # 注意中间件的注册顺序.
+
+# 注意2: 非请求上下文中,请自行创建session会话,例如:定时任务,测试脚本等.
+# from core.globals import async_db
+# async with async_db():
+#     async_db.async_get(...)
+#     async_db.session.get(...)
+#     # do something
+
+@app.on_event("startup")
+async def startup():
+    await site.db.async_run_sync(SQLModel.metadata.create_all, is_session=False)
+
+
+    pass
+
+
+
+@app.get('/')
+async def index():
+    return RedirectResponse(url=site.router_path)
