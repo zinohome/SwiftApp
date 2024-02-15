0b5aaa5a558e528755dc473b4d8e9f7526ca8239
zhangjun <ibmzhangjun@139.com>
2024-02-15 11:00:26 +0800

    Init

--- core/settings.py
+++ core/settings.py
@@ -23,19 +23,23 @@ class Settings(AmisSettings):
     host: str = '0.0.0.0'
     port: int = int(appdef.Def.Config.listenport)
     debug: bool = appdef.Def.Config.debug
-    secret_key: str = ''
+    secret_key: str = 'veheyyw6pv1nqtjsdbgt56yhn0hbhjltn42k7nlaikl4rn62bwewawq0qt9o6723'
     version: str = appdef.Def.Version
     site_title: str = appdef.Def.AppName
     site_icon: str = '/static/favicon.ico'
     language: str = appdef.Def.Config.language
     amis_cdn: str = '/static/'
-    amis_theme: str = 'cxd'
-    allow_origins: List[str] = None
+    amis_theme: str = appdef.Def.Config.amis_theme
+    allow_origins: List[str] = appdef.Def.Config.allow_origins.split(",")
+    language: str = appdef.Def.Config.language
+    database_url_async: str = appdef.Def.Config.database_url_async
 
 
-settings = Settings(_env_file=os.path.join(BASE_DIR, '.env'))
-
+#settings = Settings(_env_file=os.path.join(BASE_DIR, '.env'))
+settings = Settings()
+settings.database_url_async = appdef.Def.Config.database_url_async
 
 if __name__ == '__main__':
+    print(settings)
     print(settings.database_url_async)
     print(settings.allow_origins)
