768cfcc4cbd282bc27665853c15faeae00d14834
zhangjun <ibmzhangjun@139.com>
2024-02-16 15:29:25 +0800

    Init

--- backend/core/globals.py
+++ backend/core/globals.py
@@ -8,10 +8,11 @@
 #  @Email   : ibmzhangjun@139.com
 #  @Software: SwiftApp
 import os
+from typing import Optional
 
 from fastapi import FastAPI
 from fastapi_amis_admin.admin import HomeAdmin, DocsAdmin, ReDocsAdmin
-from fastapi_amis_admin.amis import App, ActionType, Flex, Drawer, SizeEnum, Service, AmisAPI
+from fastapi_amis_admin.amis import App, ActionType, Flex, Drawer, SizeEnum, Service, AmisAPI, InputImage, ColumnImage
 from fastapi_amis_admin.crud.utils import SqlalchemyDatabase
 from fastapi_user_auth.admin import AuthAdminSite
 from fastapi_user_auth.auth import Auth
@@ -19,6 +20,8 @@ from fastapi_user_auth.auth.schemas import SystemUserEnum
 from sqlalchemy_database import AsyncDatabase, Database
 from fastapi_user_auth.auth.backends.jwt import JwtTokenStore
 from starlette.requests import Request
+from fastapi_amis_admin.models.fields import Field
+from fastapi_user_auth.auth.models import BaseUser
 
 from core.settings import settings, Settings
 from utils.log import log as log
@@ -39,7 +42,7 @@ sync_db = Database.create(
     },
 )
 
-auth = Auth(db=async_db,token_store=JwtTokenStore(secret_key=settings.secret_key))
+auth = Auth(db=async_db, token_store=JwtTokenStore(secret_key=settings.secret_key))
 site = AuthAdminSite(settings, engine=async_db, auth=auth)
 auth = site.auth
 site.UserAuthApp.page_schema.sort = -99
