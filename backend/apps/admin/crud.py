eabfae837eccf5a99d73cff21967456149bb2262
zhangjun <ibmzhangjun@139.com>
2024-02-18 11:33:27 +0800

    Init

--- backend/apps/admin/crud.py
+++ backend/apps/admin/crud.py
@@ -9,18 +9,16 @@
 #  @Software: SwiftApp
 # 注册API
 from fastapi import APIRouter
+from fastapi_amis_admin.crud import SqlalchemyCrud
+
 from apps.admin.models.contractdetail import Contractdetail
 from core.globals import site
 
 from construct.app import App
-from utils.famupdate._sqlalchemy import SqlalchemyCrud
+
 from utils.log import log as log
 
 router = APIRouter(prefix='/contract')
-#self.pk_name: str = self.pk_name or self.model.__table__.primary_key.columns.keys()[0]
-log.debug(Contractdetail.__table__.primary_key.columns.keys()[0])
-pn = str = 'id' or Contractdetail.__table__.primary_key.columns.keys()[0]
-log.debug(pn)
 contractdetail_crud = SqlalchemyCrud(model=Contractdetail, engine=site.engine).register_crud()
 #router.include_router(contractdetail_crud.router)
 
