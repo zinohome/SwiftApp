dcd5d15f97a8f1e7bd88769ba3471aa915e2ad30
zhangjun <ibmzhangjun@139.com>
2024-06-01 16:31:26 +0800

    fast_api_amis_admin embeded

--- backend/main.py
+++ backend/main.py
@@ -11,6 +11,7 @@ from starlette.requests import Request
 from starlette.responses import RedirectResponse
 from fastapi import FastAPI
 from sqlmodel import SQLModel
+from starlette.staticfiles import StaticFiles
 
 from core.globals import site, auth
 from core.settings import settings
@@ -55,6 +56,47 @@ async def startup():
 @app.get('/')
 async def index():
     return RedirectResponse(url=site.router_path)
+
+# 1.配置 CORSMiddleware
+from starlette.middleware.cors import CORSMiddleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=settings.allow_origins,  # 允许访问的源
+    allow_credentials=True,  # 支持 cookie
+    allow_methods=["*"],  # 允许使用的请求方法
+    allow_headers=["*"]  # 允许携带的 Headers
+)
+
+# 2. 配置静态资源目录
+app.mount("/static", StaticFiles(directory="apps/static"), name="static")
+
+# 3.配置 Swagger UI CDN
+from fastapi.openapi.docs import get_swagger_ui_html, get_swagger_ui_oauth2_redirect_html, get_redoc_html
+
+@app.get("/apidocs", include_in_schema=False)
+async def custom_swagger_ui_html():
+    return get_swagger_ui_html(
+        openapi_url=app.openapi_url,
+        title=f"{app.title} - Swagger UI",
+        oauth2_redirect_url=app.swagger_ui_oauth2_redirect_url,
+        swagger_js_url="/static/swagger-ui-dist@4/swagger-ui-bundle.js",
+        swagger_favicon_url="/static/favicon.ico",
+        swagger_css_url="/static/swagger-ui-dist@4/swagger-ui.css",
+    )
+@app.get(app.swagger_ui_oauth2_redirect_url, include_in_schema=False)
+async def swagger_ui_redirect():
+    return get_swagger_ui_oauth2_redirect_html()
+
+# 4.配置 Redoc CDN
+@app.get("/apiredoc", include_in_schema=False)
+async def custom_swagger_ui_html():
+    return get_redoc_html(
+        openapi_url=app.openapi_url,
+        title=f"{app.title} - ReDoc",
+        redoc_js_url="/static/redoc/redoc.standalone.js",
+        redoc_favicon_url="/static/favicon.ico",
+        with_google_fonts=False,
+    )
 
 # 要求: 用户必须登录
 @app.get("/auth/get_user")
